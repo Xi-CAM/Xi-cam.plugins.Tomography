7d23c5bc8d97dbd833c38e25c021ab6f162724cf
ronpandolfi <ronpandolfi@gmail.com>
2018-02-23 10:59:39 -0800

    Add paths to catch plugin markers

--- setup.py
+++ setup.py
@@ -84,7 +84,9 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={'xicam.Tomography': ['*.yapsy-plugin']},
+    package_data={'xicam.Tomography': ['*.yapsy-plugin'],
+                  'xicam.Tomography.formats': ['*.yapsy-plugin'],
+                  'xicam.Tomography.processing': ['*.yapsy-plugin'], },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
