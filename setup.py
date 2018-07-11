9299d95cf74c5f81f1298325d979108c6ba37c62
Ronald Pandolfi <ronpandolfi@gmail.com>
2018-07-11 14:12:01 -0700

    add dependencies

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'qtpy'],
+    install_requires=['numpy', 'qtpy', 'scikit-image', 'vispy'],
 
     setup_requires=[],
 
